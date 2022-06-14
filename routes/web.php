6ffd901c8a1bfece0447c0d2d79986e1d04b77e4
CHARISA C. SEVILLA <101934768+charisevi@users.noreply.github.com>
2022-06-14 08:48:57 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/', function()
+{
+  return View::make('pages.home');
+});
+
+Route::get('/contact', function()
+{
+  return View::make('pages.contact');
+});
