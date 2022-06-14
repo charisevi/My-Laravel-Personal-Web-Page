b078b4b8aff065bda0d81e4227fee681ac278eab
CHARISA C. SEVILLA <101934768+charisevi@users.noreply.github.com>
2022-06-14 10:41:46 -0800

    Update header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,1 +1,16 @@
+<div class="navbar">
+  <div class="navbar-inner">
 
+    <a id="logo" style="display: none"> Charisa C. Sevilla</a>
+
+    <ul class="nav" style="display: none">
+
+      <li><a href="#home"> Home </a><li>
+      <li><a href="#CS202"> Load Balancing </a><li>
+      <li><a href="#laravelhisto"> Laravel History </a><li>
+      <li><a href="#aboutme"> About Me </a><li>
+      <li><a href="#contact"> Contact Me </a><li>
+    </ul>
+
+  </div>
+</div>
